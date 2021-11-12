801348b48e110c262018e87b69e719d66676cd3c
francescapetra <francescalaurapetra@gmail.com>
2021-11-12 14:03:59 +0100

    pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+registerLocaleData(localeIt);
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UcfirstPipe } from './ucfirst.pipe';
+import { TestPipe } from './test.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UcfirstPipe,
+    TestPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+     { provide: LOCALE_ID, useValue: 'it-IT'},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
